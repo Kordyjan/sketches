a862ebc9be8d2d1d510d08f2d8cfe3efcb88a52f
Paweł Marks <paweljmarks@gmail.com>
2024-11-27 14:18:01 +0100

    Implement SparseVec

--- sparse_vec/src/lib.rs
+++ sparse_vec/src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::mem;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct SparseVec<const CAP: usize, T> {
+    mask: usize,
+    data: Vec<T>,
+}
+
+impl<const CAP: usize, T> Default for SparseVec<CAP, T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<const CAP: usize, T> SparseVec<CAP, T> {
+    #[must_use]
+    pub fn new() -> Self {
+        Self {
+            mask: 0,
+            data: Vec::with_capacity(2),
+        }
+    }
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.mask.count_ones() as usize
+    }
+
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.mask == 0
+    }
+
+    pub fn insert(&mut self, pos: usize, elem: T) {
+        let real_pos = self.elems_before(pos);
+        self.mask |= 1 << (CAP - pos - 1);
+        self.data.insert(real_pos, elem);
+    }
+
+    #[must_use]
+    pub fn get(&self, pos: usize) -> Option<&T> {
+        if self.mask & (1 << (CAP - pos - 1)) != 0 {
+            Some(&self.data[self.elems_before(pos)])
+        } else {
+            None
+        }
+    }
+
+    pub fn remove(&mut self, pos: usize) -> Option<T> {
+        if self.mask & (1 << (CAP - pos - 1)) != 0 {
+            let real_pos = self.elems_before(pos);
+            let res = Some(self.data.remove(real_pos));
+            self.mask &= !(1 << (CAP - pos - 1));
+            res
+        } else {
+            None
+        }
+    }
+
+    #[must_use]
+    pub fn swap(&mut self, pos: usize, elem: T) -> Option<T> {
+        if self.mask & (1 << (CAP - pos - 1)) != 0 {
+            let real_pos = self.elems_before(pos);
+            let mut res = elem;
+            mem::swap(&mut self.data[real_pos], &mut res);
+            Some(res)
+        } else {
+            None
+        }
+    }
+
+    fn elems_before(&self, pos: usize) -> usize {
+        (self.mask >> (CAP - pos)).count_ones() as usize
     }
 }
---
+++ sparse_vec/src/tests.rs
@@ -0,0 +1,94 @@
+use std::collections::{HashMap, HashSet};
+
+use super::SparseVec;
+use proptest::collection::hash_map;
+use proptest::prelude::*;
+use proptest::sample::select;
+
+proptest! {
+    #[test]
+    fn len_is_correct(elems in hash_map(0usize..16, ".*", 0usize..5)) {
+        let mut sparse_vec = SparseVec::<16, String>::new();
+        let len = elems.len();
+        for (pos, elem) in elems {
+            sparse_vec.insert(pos, elem);
+        }
+        prop_assert_eq!(len, sparse_vec.len());
+    }
+
+    #[test]
+    fn elements_can_be_retrieved(elems in hash_map(0usize..16, ".*", 0usize..5)) {
+        let mut sparse_vec = SparseVec::<16, String>::new();
+        for (pos, elem) in elems.iter() {
+            sparse_vec.insert(*pos, elem.clone());
+        }
+        for (pos, elem) in elems {
+            prop_assert_eq!(Some(&elem), sparse_vec.get(pos));
+        }
+    }
+
+    #[test]
+    fn elements_can_be_overwritten((elems, selected) in map_with_selected(5)) {
+        let mut sparse_vec = SparseVec::<16, String>::new();
+        for (pos, elem) in elems.iter() {
+            sparse_vec.insert(*pos, elem.clone());
+        }
+
+        let new_value = "new value".to_string();
+
+        sparse_vec.insert(selected, new_value.clone());
+        prop_assert_eq!(Some(&new_value), sparse_vec.get(selected));
+
+    }
+
+    #[test]
+    fn elemenets_can_be_swapped((elems, selected) in map_with_selected(5)) {
+        let mut sparse_vec = SparseVec::<16, String>::new();
+        for (pos, elem) in elems.iter() {
+            sparse_vec.insert(*pos, elem.clone());
+        }
+
+        let new_value = "new value".to_string();
+
+        let old_value = sparse_vec.swap(selected, new_value.clone());
+        prop_assert_eq!(Some(&new_value), sparse_vec.get(selected));
+        prop_assert_eq!(elems.get(&selected).map(String::clone), old_value)
+    }
+
+    #[test]
+    fn elements_can_be_removed((elems, selected) in map_with_selected(5)) {
+        let mut sparse_vec = SparseVec::<16, String>::new();
+        for (pos, elem) in elems.iter() {
+            sparse_vec.insert(*pos, elem.clone());
+        }
+
+        let old_value = sparse_vec.remove(selected);
+        prop_assert_eq!(None, sparse_vec.get(selected));
+        prop_assert_eq!(elems.get(&selected).map(String::clone), old_value)
+    }
+
+    #[test]
+    fn not_inserted_elements_are_empty(elems in hash_map(0usize..16, ".*", 0usize..5)) {
+        let mut sparse_vec = SparseVec::<16, String>::new();
+        for (pos, elem) in elems.iter() {
+            sparse_vec.insert(*pos, elem.clone());
+        }
+
+        for id in (0usize..16).collect::<HashSet<_>>()
+            .difference(&elems.keys()
+            .map(|u| *u)
+            .collect::<HashSet<_>>()) {
+            prop_assert_eq!(None, sparse_vec.get(*id));
+            ()
+        }
+    }
+}
+
+prop_compose! {
+    fn map_with_selected(elem_num: usize)
+        (elems in hash_map(0usize..16, ".*", 1..elem_num))
+            (selected in select(elems.keys().map(|u| *u).collect::<Vec<usize>>()), elems in Just(elems))
+            -> (HashMap<usize, String>, usize)  {
+                (elems, selected)
+        }
+}
